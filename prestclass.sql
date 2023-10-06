0a90c5513c4ed489667bc311b4cf8cfd3a87298f
alzatemiguel284 <alzatemiguel284@gmail.com>
2023-10-06 10:16:40 -0500

    base de datos

---
+++ prestclass.sql
@@ -0,0 +1,226 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 06-10-2023 a las 16:08:18
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `prestclass`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos`
+--
+
+CREATE TABLE `prestamos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `usuarios_id` int(10) UNSIGNED NOT NULL,
+  `estado_prestamo` varchar(45) NOT NULL,
+  `fecha_prestamo` date NOT NULL,
+  `fecha_reserva` datetime NOT NULL DEFAULT current_timestamp(),
+  `hora_inicial` int(2) NOT NULL,
+  `hora_final` int(2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos_recursos`
+--
+
+CREATE TABLE `prestamos_recursos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `prestamo_id` int(11) UNSIGNED NOT NULL,
+  `recurso_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `prestamos_salas`
+--
+
+CREATE TABLE `prestamos_salas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `prestamos_id` int(11) UNSIGNED NOT NULL,
+  `salas_id` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `recursos`
+--
+
+CREATE TABLE `recursos` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `estado_recursos` int(45) NOT NULL,
+  `descripción` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `salas`
+--
+
+CREATE TABLE `salas` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `estado_salas` varchar(300) NOT NULL,
+  `nombre` varchar(50) DEFAULT 'sala',
+  `tipo_de_sala` varchar(50) NOT NULL,
+  `descripcion` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(45) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `contraseña` varchar(40) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `usuario`, `correo`, `contraseña`) VALUES
+(3, 'Miguel', 'Miguel', 'miguel@gmail.com', '$2a$10$CGOxjDGolodz0nFe6lg8neJwriViQlGmN'),
+(4, 'Miguel', 'Miguel1', 'miguel1@gmail.com', '$2a$10$AOdQsMkb2I2d2jKlHBeG0OiPecUP2QFSw');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuarios_id` (`usuarios_id`);
+
+--
+-- Indices de la tabla `prestamos_recursos`
+--
+ALTER TABLE `prestamos_recursos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `prestamo_id` (`prestamo_id`),
+  ADD KEY `recurso_id` (`recurso_id`);
+
+--
+-- Indices de la tabla `prestamos_salas`
+--
+ALTER TABLE `prestamos_salas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `prestamos_id` (`prestamos_id`),
+  ADD KEY `salas_id` (`salas_id`);
+
+--
+-- Indices de la tabla `recursos`
+--
+ALTER TABLE `recursos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `salas`
+--
+ALTER TABLE `salas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos_recursos`
+--
+ALTER TABLE `prestamos_recursos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `prestamos_salas`
+--
+ALTER TABLE `prestamos_salas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `recursos`
+--
+ALTER TABLE `recursos`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `salas`
+--
+ALTER TABLE `salas`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `prestamos`
+--
+ALTER TABLE `prestamos`
+  ADD CONSTRAINT `prestamos_ibfk_1` FOREIGN KEY (`usuarios_id`) REFERENCES `usuarios` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `prestamos_recursos`
+--
+ALTER TABLE `prestamos_recursos`
+  ADD CONSTRAINT `prestamos_recursos_ibfk_1` FOREIGN KEY (`recurso_id`) REFERENCES `recursos` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_recursos_ibfk_2` FOREIGN KEY (`prestamo_id`) REFERENCES `prestamos` (`id`) ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `prestamos_salas`
+--
+ALTER TABLE `prestamos_salas`
+  ADD CONSTRAINT `prestamos_salas_ibfk_1` FOREIGN KEY (`salas_id`) REFERENCES `salas` (`id`) ON UPDATE CASCADE,
+  ADD CONSTRAINT `prestamos_salas_ibfk_2` FOREIGN KEY (`prestamos_id`) REFERENCES `prestamos` (`id`) ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
