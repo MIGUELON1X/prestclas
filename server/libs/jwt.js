e15c6b3c19ef0df9b317c076791117fe801d4e1b
alzatemiguel284 <alzatemiguel284@gmail.com>
2023-10-09 23:09:43 -0500

    userToken y AdminToken

--- server/libs/jwt.js
+++ server/libs/jwt.js
@@ -1,7 +1,20 @@
+import { TOKEN_SECRET, ADMIN_TOKEN_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 
-export const createAccessToken = (payload) => {
-    return new Promise ((resolve, reject) => {
-        
-    })
-}
+export const createUserToken = (payload) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+};
+
+export const createAdminToken = (payload) => {
+  return new Promise((resolve, reject) => {
+    jwt.sign(payload, ADMIN_TOKEN_SECRET, { expiresIn: "1d" }, (err, token) => {
+      if (err) reject(err);
+      resolve(token);
+    });
+  });
+};
